f0d7f1524b78042a160ccc9c774f6cfc87534801
paru skyovich <white47white45@gmail.com>
2025-06-16 23:33:43 +0200

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,14 @@
-#include "../nimbus.h"
+#include "nimbus_c_api.h"
 
 int main() {
- NL_DEBUG("Test"); 
- NL_INFO("Test");
- NL_WARN("Test");
- NL_ERROR("Test");
- NL_CRITICAL("Test");
- NL_FATAL("Test");
- return 0;
+    nl_setup_logging("log_dir_name");
+
+    NL_LOG(NL_DEBUG_LOG, "This is a debug message.");
+    NL_LOG(NL_INFO_LOG, "Hello from C!");
+    NL_LOG(NL_WARN_LOG, "This is a warning.");
+    NL_LOG(NL_ERROR_LOG, "An error occurred: code %d", 404);
+    NL_LOG(NL_CRITICAL_LOG, "Critical issue detected.");
+    NL_LOG(NL_FATAL_LOG, "Fatal crash imminent!");
+
+    return 0;
 }
